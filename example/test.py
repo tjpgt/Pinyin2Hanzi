d8d9bca1db78837c0c7572e5c480f695e948f24b
chenchunwei <chenchunwei@sansi.com>
2022-09-19 16:45:48 +0800

    feat: 增加hmm的输出

--- example/test.py
+++ example/test.py
@@ -6,6 +6,10 @@ sys.path.append('..')
 from Pinyin2Hanzi import DefaultDagParams
 from Pinyin2Hanzi import dag
 from Pinyin2Hanzi import simplify_pinyin
+from Pinyin2Hanzi import DefaultHmmParams
+from Pinyin2Hanzi import viterbi
+
+hmmparams = DefaultHmmParams()
 
 dagparams = DefaultDagParams()
 
@@ -15,6 +19,7 @@ with open('./a.txt', 'r') as f:
 result=[]
 word=""
 final=""
+hhfinal=""
 for char in str:
     if char.isalpha():
         word+=char
@@ -29,14 +34,27 @@ for char in str:
                     print('error:', ' '.join(result))
                 else:
                     final+=''.join(x[0].path)
+                y =  viterbi(hmm_params=hmmparams, observations=tuple(result), path_num = 2, log = True)
+                if(len(y)==0):
+                    print('error:', ' '.join(result))
+                else:
+                    hhfinal+=''.join(y[0].path)
                 result=[]
             final+=char
+            hhfinal+=char
 if len(word)>0:
     result.append(simplify_pinyin(word))
 if len(result)>0:
     x = dag(dagparams, result)
     if(len(x)==0):
-        pass
+        print('error:', ' '.join(result))
     else:
         final+=''.join(x[0].path)
+    y =  viterbi(hmm_params=hmmparams, observations=tuple(result), path_num = 2, log = True)
+    if(len(y)==0):
+        print('error:', ' '.join(result))
+    else:
+        hhfinal+=''.join(y[0].path)
 print(final)
+print("--------------------------------------")
+print(hhfinal)
