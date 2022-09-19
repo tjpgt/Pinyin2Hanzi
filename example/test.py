cfc5227a4db6b3362f2444cd485105b2c7f3c46f
tjpgt <602950305@qq.com>
2022-09-19 14:47:46 +0800

    Create test.py

---
+++ example/test.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from __future__ import (print_function, unicode_literals)
+
+import sys
+sys.path.append('..')
+
+from Pinyin2Hanzi import DefaultDagParams
+from Pinyin2Hanzi import dag
+
+dagparams = DefaultDagParams()
+
+with open('./a.txt', 'r') as f:
+    str=f.read()
+
+result=[]
+word=""
+final=""
+for char in str:
+    if char.isalpha():
+        word+=char
+    else:
+        if len(word)>0:
+            result.append(word)
+        word=""
+        if(char!=" "):
+            if len(result)>0:
+                x = dag(dagparams, result)
+                final+=''.join(x[0].path)
+                result=[]
+            final+=char
+print(final)
